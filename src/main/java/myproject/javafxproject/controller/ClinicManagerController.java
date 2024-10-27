0cd9ec8aec1cfd53cc3dd5429d4bc1d4c886c393
croaa <croaa1ruizr@gmail.com>
2024-10-27 18:27:14 -0400

    some code

--- src/main/java/myproject/javafxproject/controller/ClinicManagerController.java
+++ src/main/java/myproject/javafxproject/controller/ClinicManagerController.java
@@ -5,15 +5,21 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import myproject.javafxproject.model.clinic.Patient;
 import myproject.javafxproject.model.clinic.Timeslot;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
 
 public class ClinicManagerController {
 
     @FXML
-    protected ComboBox<String> timeslotComboBox, timeslotComboBox2;
+    protected ComboBox<String> timeslotComboBox, timeslotComboBox2, providerComboBox;
 
     @FXML
     protected void initialize() {
@@ -27,6 +33,24 @@ public class ClinicManagerController {
             timeslotComboBox2.getItems().add(slot.toString());
         }
     }
+    @FXML
+    protected void loadProviders(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("UJP Files", "*.ujp"));
+        fileChooser.setTitle("Select Provider File");
+        File selectedFile = fileChooser.showOpenDialog(null);
 
+        if(selectedFile != null){
+            providerComboBox.getItems().clear();
+            try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))){
+                String line;
+                while((line = reader.readLine()) != null){
+                    providerComboBox.getItems().add(line.trim());
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
